3db7b4cf4b31dbaaa0d1c3730f6d1a60ac10606e
Raushan Kumar <raushan9862270880@gmail.com>
2025-04-24 17:41:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,15 +24,12 @@ st.markdown("""
     <style>
         body, h1, h2, h3, h4, h5, h6 {
             font-family: 'Raleway', Arial, Helvetica, sans-serif;
-            margin: 0; /* Remove default margin */
-            padding: 0; /* Remove default padding */
         }
         .header-container {
             display: flex;
             justify-content: center; /* Centers the image horizontally */
             align-items: center; /* Centers the image vertically */
-            height: 100vh; /* Full viewport height */
-            margin: 0; /* Remove any margin around the container */
+            height: 700px; /* Adjust as needed */
         }
         .header-img {
             width: 300px; /* Set a fixed width */
@@ -81,6 +78,7 @@ st.markdown("""
         <p>Detect tumor regions from MRI scans using AI</p>
     </div>
 """, unsafe_allow_html=True)
+
 
 
 
